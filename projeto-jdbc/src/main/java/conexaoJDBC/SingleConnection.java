16072230ea10b8c932137d00263fc2ac93b5a571
Daniel Penelva <d4n.andrade@gmail.com>
2022-12-02 23:28:20 -0300

    Criando conexão com o banco de dados

---
+++ projeto-jdbc/src/main/java/conexaoJDBC/SingleConnection.java
@@ -0,0 +1,43 @@
+package conexaoJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	
+	private static String url = "jdbc:postgresql://localhost:5433/projetoJDBC"; // referencia o banco de dados postgresql
+	private static String password = "admin"; // referencia a senha do usuario
+	private static String user = "postgres"; // referencia o usuario
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		try {
+			if(connection == null) {
+				/* Carregar o drive do JDBC*/
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false); // Para não comitar automaticamente
+				
+				System.out.println("Banco conectado!");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+
+}
---
+++ projeto-jdbc/src/test/java/conexaoJDBCTest/SingleConnectionTest.java
@@ -0,0 +1,14 @@
+package conexaoJDBCTest;
+
+import org.junit.Test;
+
+import conexaoJDBC.SingleConnection;
+
+public class SingleConnectionTest {
+	
+	@Test
+	public void initBanco() {
+		SingleConnection.getConnection();
+	}
+
+}
