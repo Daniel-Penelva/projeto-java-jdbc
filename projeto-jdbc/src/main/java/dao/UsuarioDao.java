9cee8232d10f2ecababb2793f89aec61236ee9ff
Daniel Penelva <d4n.andrade@gmail.com>
2022-12-05 19:34:27 -0300

    Criando consulta sql ao banco de dados

--- projeto-jdbc/src/main/java/dao/UsuarioDao.java
+++ projeto-jdbc/src/main/java/dao/UsuarioDao.java
@@ -2,32 +2,41 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import conexaoJDBC.SingleConnection;
 import model.Usuario;
 
 /**
- * A função do DAO é gerenciar os objetos do banco de dados.
- * Para cada tipo de entidade cria-se um DAO.
- * A classe de conexão tem um vinculo com a classe dao para que seja possivel o acesso ao BD.
- * Portanto, o UsuarioDao precisa de um connection, isto é, ele é ativado através do método getConnection().
- * É na classe dao que vai ser manipulado a linguagem sql, através do comando "preparedStatement".
+ * A função do DAO é gerenciar os objetos do banco de dados. 
+ * Para cada tipo de entidade cria-se um DAO. 
+ * A classe de conexão tem um vinculo com a classe dao para que seja possivel o acesso ao BD. 
+ * Portanto, o UsuarioDao precisa de um connection, isto é, ele é ativado através do método getConnection(). 
+ * É na classe dao que vai ser manipulado a linguagem sql, através do comando "preparedStatement". 
  * Então, o sql preparado vai para a connection que terá a função de inserir no BD.
- * */
+ * ResultSet é um objeto capaz de guardar e buscar resultados no banco. 
+ * Todos os dados criados e manipulados estarão dentro do ResultSet.
+ * No UsuarioDAO é feito um "execute()" para retornar um boolean, entretanto, com o ResultSet
+ * é usado o "executeQuery()", uma vez que retorna todos os resultados da consulta.
+ * Para extrair o resultado linha por linha dentro do resultSet é add o objeto usuário correspondente para ele.
+ */
 
 public class UsuarioDao {
 
 	private Connection connection;
-	
+
 	public UsuarioDao() {
 		connection = SingleConnection.getConnection();
 	}
-	
+
+	// Cadastrar um usuário
 	public void salvar(Usuario usuario) {
-		
+
 		String sql = "INSERT INTO USUARIO(id, nome, email)VALUES(?,?,?)";
-		
+
 		try {
 			PreparedStatement inserir = connection.prepareStatement(sql);
 			inserir.setLong(1, usuario.getId());
@@ -36,7 +45,7 @@ public class UsuarioDao {
 			inserir.execute();
 			connection.commit();
 			System.out.println("Cadastrado com sucesso!");
-			
+
 		} catch (SQLException e) {
 			try {
 				connection.rollback();
@@ -46,5 +55,50 @@ public class UsuarioDao {
 			e.printStackTrace();
 		}
 	}
-	
+
+	// Listar os usuários
+	public List<Usuario> listar() throws Exception {
+		List<Usuario> list = new ArrayList<Usuario>();
+
+		String sql = "select * from usuario";
+
+		PreparedStatement buscar = connection.prepareStatement(sql);
+		ResultSet resultado = buscar.executeQuery();
+
+		// Enquanto existir usuario add na lista retorna os valores.
+		while (resultado.next()) {
+			Usuario usuario = new Usuario();
+			usuario.setId(resultado.getLong("id"));
+			usuario.setNome(resultado.getString("nome"));
+			usuario.setEmail(resultado.getString("email"));
+
+			list.add(usuario);
+		}
+		
+		System.out.println("Lista mostrada com sucesso!");
+		
+		return list;
+	}
+
+	// Buscar o usuário através do id
+	public Usuario buscar(Long id) throws Exception {
+
+		Usuario buscarUsuario = new Usuario();
+
+		String sql = "select * from usuario where id = " + id;
+
+		PreparedStatement buscar = connection.prepareStatement(sql);
+		ResultSet resultado = buscar.executeQuery();
+
+		// Enquanto estiver usuario retorna o valor
+		while (resultado.next()) {
+
+			buscarUsuario.setId(resultado.getLong("id"));
+			buscarUsuario.setNome(resultado.getString("nome"));
+			buscarUsuario.setEmail(resultado.getString("email"));
+		}
+
+		return buscarUsuario;
+	}
+
 }
--- projeto-jdbc/src/test/java/conexaoJDBCTest/TestDAO.java
+++ projeto-jdbc/src/test/java/conexaoJDBCTest/TestDAO.java
@@ -1,5 +1,7 @@
 package conexaoJDBCTest;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import dao.UsuarioDao;
@@ -11,11 +13,49 @@ public class TestDAO {
 	public void testeCadastrarBanco() {
 		Usuario usu = new Usuario();
 		UsuarioDao usuDao = new UsuarioDao();
-		
-		usu.setId(4L);
-		usu.setNome("Eduarda Andrade");
-		usu.setEmail("eduarda@gmail.com");
-		
+
+		usu.setId(6L);
+		usu.setNome("Miranda Nunes");
+		usu.setEmail("miranda@gmail.com");
+
 		usuDao.salvar(usu);
+	}
+
+	@Test
+	public void testeListarBanco() {
+		UsuarioDao usuDao = new UsuarioDao();
+		try {
+			List<Usuario> list = usuDao.listar();
+
+			for (Usuario usu : list) {
+				// imprimi o toString
+				System.out.println(usu);
+				System.out.println("------------------------------------");
+			}
+
+			for (Usuario usu : list) {
+				// imprimi o id e o nome
+				System.out.println("Nome: " + usu.getNome() + " - id: " + usu.getId());
+				System.out.println("------------------------------------");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testeBuscarDados() {
+		UsuarioDao usuDao = new UsuarioDao();
+
+		try {
+			Usuario usu = usuDao.buscar(4L);
+			System.out.println(usu);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
 	}
 }
